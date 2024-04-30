c1796427b519f52c7b09c452190a64d2dc388989
yasmen_zuheer <smsmabdallah43@gamil.com>
2024-04-30 11:55:51 +0300

    modefiy

--- Event-exer/Exercise1.js
+++ Event-exer/Exercise1.js
@@ -7,3 +7,18 @@ div.onmouseover=function(){
 div.onmouseout=function(){
     div.innerHTML="hello world !";
 }
+
+const button = document.getElementById('button');
+let stopped = false;
+ 
+function move(isReturning) {
+    debugger;
+ const width = button.parentNode.clientWidth;
+ const left = parseInt(button.style.left , 10) || 0;
+ if (!stopped) {
+    button.style.left = (isReturning ? left - 1 : left + 1) + 'px';
+    setTimeout(() => move ((isReturning && left > 0) || left === width - button.clientWidth ), 10);
+ };
+};
+ 
+move();
